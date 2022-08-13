bc17a97cd28415066884842ecba290294f92a5a1
Cano <developer.sasazono@gmail.com>
2022-08-13 18:36:56 +0900

    convert soft break in markdown to <br> (#7)

--- src/core/parser.ts
+++ src/core/parser.ts
@@ -1,8 +1,10 @@
+import remarkBreaks from "remark-breaks";
 import remarkParse from "remark-parse";
 import { unified } from "unified";
 import { remarkRemovePosition } from "../plugin/remark-remove-position";
 
 export const markdownParser = unified()
   .use(remarkParse) // parse markdown text to mdast
+  .use(remarkBreaks) // convert soft break in markdown to <br>
   .use(remarkRemovePosition) // remove position property
   .freeze();
