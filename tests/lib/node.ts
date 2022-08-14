97e471553ec0625575b0f6991e4e90e1a07a08eb
Cano <contact.sasasaky@gmail.com>
2022-08-14 21:56:08 +0900

    using u fn

--- tests/lib/node.ts
+++ tests/lib/node.ts
@@ -8,55 +8,34 @@ import {
   Text as TextNode,
 } from "mdast";
 import { Node } from "unist";
+import { u } from "unist-builder";
 
 type TestNodeBuilder<N extends Node> = (props: Omit<N, "type">) => N;
 
 export const Root: TestNodeBuilder<RootNode> = ({ data, children }) => {
-  return {
-    type: "root",
-    data: data,
-    children,
-  };
+  return u("root", { data }, children);
 };
 
 export const Paragraph: TestNodeBuilder<ParagraphNode> = ({ children }) => {
-  return {
-    type: "paragraph",
-    children,
-  };
+  return u("paragraph", children);
 };
 
 export const Heading: TestNodeBuilder<HeadingNode> = ({ depth, children }) => {
-  return {
-    type: "heading",
-    depth,
-    children,
-  };
+  return u("heading", { depth }, children);
 };
 
 export const Emphasis: TestNodeBuilder<EmphasisNode> = ({ children }) => {
-  return {
-    type: "emphasis",
-    children,
-  };
+  return u("emphasis", children);
 };
 
 export const Strong: TestNodeBuilder<StrongNode> = ({ children }) => {
-  return {
-    type: "strong",
-    children,
-  };
+  return u("strong", children);
 };
 
 export const Text: TestNodeBuilder<TextNode> = ({ value }) => {
-  return {
-    type: "text",
-    value,
-  };
+  return u("text", value);
 };
 
 export const Break: TestNodeBuilder<BreakNode> = () => {
-  return {
-    type: "break",
-  };
+  return u("break");
 };
