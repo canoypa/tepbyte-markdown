a03df51c27247e16ce0d31cad30c07c929ae4305
Cano <developer.sasazono@gmail.com>
2023-01-01 20:23:09 +0900

    Fix tests (#19)

---
+++ tests/link.spec.ts
@@ -0,0 +1,36 @@
+import { parseMarkdown } from "../src";
+
+describe("link", () => {
+  it("autolink", async () => {
+    const source = `
+https://example.com
+www.example.com
+`;
+
+    const result = await parseMarkdown(source);
+
+    expect(result).toEqual({
+      type: "root",
+      children: [
+        {
+          type: "paragraph",
+          children: [
+            {
+              type: "link",
+              title: null,
+              url: "https://example.com",
+              children: [{ type: "text", value: "https://example.com" }],
+            },
+            { type: "break" },
+            {
+              type: "link",
+              title: null,
+              url: "http://www.example.com",
+              children: [{ type: "text", value: "www.example.com" }],
+            },
+          ],
+        },
+      ],
+    });
+  });
+});
